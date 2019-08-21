154d2dd48b96eed7fdb537a5702c88e4dbdff80b
johannes karoff <johannes@karoff.net>
2019-08-21 15:56:20 +0200

    add error logging to sns

--- run.sh
+++ run.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-set -e
+set -o pipefail
 
 if [ -z "${AWS_BUCKET}" ]; then
   echo "You need to set the AWS_BUCKET environment variable."
   exit 1
 fi
 
-if [ -z "${PREFIX}" ]; then
-  echo "You need to set the PREFIX environment variable."
+if [ -z "${AWS_BUCKET_PREFIX}" ]; then
+  echo "You need to set the AWS_BUCKET_PREFIX environment variable."
+  exit 1
+fi
+
+if [ -z "${AWS_SNS_TOPIC_ARN}" ]; then
+  echo "You need to set the AWS_BUCKET_PREFIX environment variable."
   exit 1
 fi
 
@@ -36,7 +41,12 @@ POSTGRES_HOST_OPTS=""
 
 echo "Starting dump of ${PGDATABASE} database(s) from ${PGHOST}..."
 
-pg_dump $PGDUMP_OPTIONS $POSTGRES_HOST_OPTS $PGDUMP_DATABASE | aws s3 cp - s3://$AWS_BUCKET/$PREFIX/$(date +"%Y")/$(date +"%m")/$(date +"%d").dump || exit 2
+pg_dump $PGDUMP_OPTIONS | aws s3 cp - s3://$AWS_BUCKET/$AWS_BUCKET_PREFIX/$(date +"%Y")/$(date +"%m")/$(date +"%d").sql
+error=$?
+if [[ $error != 0 ]]; then
+    echo "Error in backup, will send to sns topic!"
+    aws sns publish --topic-arn "${AWS_SNS_TOPIC_ARN}" --subject "Postgres Backup failure - $PGHOST" --message "Failed to dump backup to S3. Sorry."
+fi
 
 echo "Done!"
 
